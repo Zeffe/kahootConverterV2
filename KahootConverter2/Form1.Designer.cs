5da3447b03150a9390351c8da703a929645c6a8e
Zeffer <zeffer5789@gmail.com>
2015-12-19 00:28:08 -0600

    First Commit

---
+++ KahootConverter2/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace KahootConverter2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.btnScrape = new System.Windows.Forms.Button();
+            this.nmQuestions = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.nmQuestions)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSite
+            // 
+            this.txtSite.Location = new System.Drawing.Point(6, 19);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(86, 20);
+            this.txtSite.TabIndex = 900;
+            // 
+            // btnScrape
+            // 
+            this.btnScrape.Location = new System.Drawing.Point(6, 71);
+            this.btnScrape.Name = "btnScrape";
+            this.btnScrape.Size = new System.Drawing.Size(86, 22);
+            this.btnScrape.TabIndex = 0;
+            this.btnScrape.Text = "Scrape";
+            this.btnScrape.UseVisualStyleBackColor = true;
+            this.btnScrape.Click += new System.EventHandler(this.btnScrape_Click);
+            // 
+            // nmQuestions
+            // 
+            this.nmQuestions.Location = new System.Drawing.Point(6, 45);
+            this.nmQuestions.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nmQuestions.Name = "nmQuestions";
+            this.nmQuestions.Size = new System.Drawing.Size(86, 20);
+            this.nmQuestions.TabIndex = 901;
+            this.nmQuestions.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nmQuestions);
+            this.groupBox1.Controls.Add(this.btnScrape);
+            this.groupBox1.Controls.Add(this.txtSite);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 102);
+            this.groupBox1.TabIndex = 902;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data Scraping";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLogin);
+            this.groupBox2.Controls.Add(this.txtPass);
+            this.groupBox2.Controls.Add(this.txtUser);
+            this.groupBox2.Location = new System.Drawing.Point(118, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(136, 102);
+            this.groupBox2.TabIndex = 903;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kahoot Login";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(6, 19);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(124, 20);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(6, 45);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(124, 20);
+            this.txtPass.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(6, 71);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(124, 22);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(18, 141);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(547, 322);
+            this.webBrowser1.TabIndex = 904;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 475);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Kahoot Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nmQuestions)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSite;
+        private System.Windows.Forms.Button btnScrape;
+        private System.Windows.Forms.NumericUpDown nmQuestions;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ KahootConverter2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using Microsoft.Win32;
+using mshtml;
+
+namespace KahootConverter2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String data, title, kData;
+        String[] placeText = new String[10];
+        Button[] button = new Button[50];
+        String[] qData;
+        String[] aData;
+        TextBox[] textboxA = new TextBox[50];
+        int count = 0;
+        int cur;
+
+        public void placeHolder(TextBox textbox, String text, Button button2)
+        {
+            placeText[count] = text;
+            textbox.Text = text;
+            if (button2 != null)
+            {
+                button[count] = button2;
+            }
+            textboxA[count] = textbox;
+            textbox.ForeColor = SystemColors.WindowFrame;
+            textboxA[count].KeyDown += new KeyEventHandler(Enter2);
+            textboxA[count].Enter += new System.EventHandler(onEnter);
+            textboxA[count].Leave += new System.EventHandler(onLeave);
+            count++;
+        }
+
+        public void Enter2(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && button[cur] != null)
+            {
+                button[cur].PerformClick();
+            }
+        }
+
+        public void onEnter(object sender, EventArgs e)
+        {
+            TextBox textbox = sender as TextBox;
+            for (int i = 0; i < placeText.Length; i++)
+            {
+                if (textbox.Text == placeText[i])
+                {
+                    textbox.ForeColor = SystemColors.WindowText;
+                    textbox.Text = "";
+                    cur = i;
+                }
+            }
+        }
+
+        public void onLeave(object sender, EventArgs e)
+        {
+            TextBox textbox = sender as TextBox;
+            if (textbox.Text == "")
+            {
+                textbox.ForeColor = SystemColors.WindowFrame;
+                textbox.Text = placeText[cur];
+            }
+        }
+
+        void getData(string ID)
+        {
+            string url = "https://quizlet.com/" + ID + "/test?mult_choice=on&prompt-with=1&limit=" + nmQuestions.Text;
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    data = client.DownloadString(url);
+                    kData = client.DownloadString("https://create.kahoot.it/#login");
+                }
+            } catch
+            {
+                MessageBox.Show("Invalid ID", "Error");
+            }
+
+            qData = data.Split(new string[] { "<span class='TermText qDef lang-en'>"}, StringSplitOptions.None);
+            aData = data.Split(new string[] { "<span class='TermText qWord lang-en'>" }, StringSplitOptions.None);
+            title = data.Split(new string[] { "<title>Test: " }, StringSplitOptions.None)[1].Split(new string[] { " | Quizlet</title>" }, StringSplitOptions.None)[0];
+            this.Text = "Kahoot Converter - " + title + " - " + nmQuestions.Text + " Questions";
+        }
+
+        private static void WebBrowserVersionEmulation()
+        {
+            const string BROWSER_EMULATION_KEY =
+            @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+            //
+            // app.exe and app.vshost.exe
+            String appname = Process.GetCurrentProcess().ProcessName + ".exe";
+            //
+            // Webpages are displayed in IE9 Standards mode, regardless of the !DOCTYPE directive.
+            const int browserEmulationMode = 11001;
+
+            RegistryKey browserEmulationKey =
+            Registry.CurrentUser.OpenSubKey(BROWSER_EMULATION_KEY, RegistryKeyPermissionCheck.ReadWriteSubTree) ??
+            Registry.CurrentUser.CreateSubKey(BROWSER_EMULATION_KEY);
+
+            if (browserEmulationKey != null)
+            {
+                browserEmulationKey.SetValue(appname, browserEmulationMode, RegistryValueKind.DWord);
+                browserEmulationKey.Close();
+            }
+        }
+
+        // Form1 Load.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            placeHolder(txtSite, "Quizlet ID", btnScrape);
+            placeHolder(txtUser, "Kahoot User", null);
+            placeHolder(txtPass, "Kahoot Pass", btnLogin);
+            WebBrowserVersionEmulation();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if ((txtUser.Text != "" || txtUser.Text != "Kahoot User") && (txtPass.Text != "" || txtPass.Text != "Kahoot Pass"))
+            {
+                webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+                webBrowser1.DocumentText = kData;
+                webBrowser1.Navigate("https://create.kahoot.it/#login");
+            } else
+            {
+                MessageBox.Show("Please ensure both username and password are filled out.", "Error");
+            }
+        }
+
+        void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlElement head = webBrowser1.Document.GetElementsByTagName("head")[0];
+            HtmlElement scriptEl = webBrowser1.Document.CreateElement("script");
+            IHTMLScriptElement element = (IHTMLScriptElement)scriptEl.DomElement;
+            element.text = "function logIn() { var inpObj = document.getElementsByTagName('input'); inpObj[0].value = \"" + txtUser.Text + "\"; inpObj[1].value = \"" + txtPass.Text + "\";var e = jQuery.Event(\"keypress\");e.which = 13; e.keyCode = 13; var btns = document.getElementsByClassName(\"btn register\"); $(btns[1]).trigger(e);}";
+            head.AppendChild(scriptEl);
+            webBrowser1.Document.InvokeScript("logIn");
+        }
+
+        private void btnScrape_Click(object sender, EventArgs e)
+        {
+            getData(txtSite.Text);
+        }
+    }
+}
